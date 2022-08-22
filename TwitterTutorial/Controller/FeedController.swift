3af18f92302a318675a40f82517ad396e1845f05
cantsleep <cant_sleep@naver.com>
2022-08-22 23:08:29 +0900

    0822 - Nav, TabBar

--- TwitterTutorial/Controller/FeedController.swift
+++ TwitterTutorial/Controller/FeedController.swift
@@ -14,7 +14,6 @@ class FeedController: UIViewController{
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         configureUI()
     
         // Do any additional setup after loading the view.
